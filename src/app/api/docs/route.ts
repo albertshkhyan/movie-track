366f1585c28d1851e9eb525b2792dcf61ae5c621
albertshkhyan <alikshkhyan@gmail.com>
2024-10-28 06:41:58 +0400

    add app/api/docs for prevent vercel issue

---
+++ src/app/api/docs/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+  const filePath = path.join(process.cwd(), 'src/docs/swagger.yaml');
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  return new NextResponse(fileContents, {
+    headers: { 'Content-Type': 'text/yaml' },
+  });
+}
